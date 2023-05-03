495d2022bfd58e36030ac441f9e517f65c8e2013
oliver7do <afristreaming@gmail.com>
2023-05-03 16:05:26 +0200

    Troisième commit

---
+++ test1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+    <body>
+    Je veux manje!
+<a href="https://github.com/">github</a><br>>
+    <a href="http://localhost/pr03052023/test.php">test.php</a><br>
+
+        <a href=".*/test.php">test.php</a>
+    </body>
+</html>
